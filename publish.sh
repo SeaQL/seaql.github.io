51f6e796fec9bc2d9f7a5ca1a27dd55269c333a6
Chris Tsang <tyt2y7@gmail.com>
2021-07-03 16:22:24 +0800

    publish.sh

--- publish.sh
+++ publish.sh
@@ -4,5 +4,6 @@ rm -r ../docs/SeaORM
 mv build ../docs/SeaORM
 cd ..
 git add docs
-git commit -m 'Update Docs'
-git push
+git commit -m 'Publish Docs'
+git push --force origin master:gh-pages
+git reset --hard HEAD~1
