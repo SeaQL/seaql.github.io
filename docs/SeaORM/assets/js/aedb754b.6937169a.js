aaf059cac8ad0b42d410ec3343323f256d13be51
Chris Tsang <tyt2y7@gmail.com>
2021-07-03 16:34:12 +0800

    Remove built from docs

--- docs/SeaORM/assets/js/aedb754b.6937169a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunksea_orm=self.webpackChunksea_orm||[]).push([[287],{3667:function(e){"use strict";e.exports=JSON.parse('{"permalink":"/SeaORM/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
