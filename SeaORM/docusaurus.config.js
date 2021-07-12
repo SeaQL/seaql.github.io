ef92860d7c5825309c6dbe43857ce1945a06ad92
Billy Chan <ccw.billy.123@gmail.com>
2021-07-12 14:21:20 +0800

    prism support toml & rust highlighting

--- SeaORM/docusaurus.config.js
+++ SeaORM/docusaurus.config.js
@@ -92,6 +92,10 @@ module.exports = {
     prism: {
       theme: require('prism-react-renderer/themes/github'),
       darkTheme: require('prism-react-renderer/themes/dracula'),
+      additionalLanguages: [
+        'toml',
+        'rust',
+      ],
     },
     hideableSidebar: true,
   },
