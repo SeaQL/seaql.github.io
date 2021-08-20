f463f860486a6cacc05108906a4cd395030877fc
Chris Tsang <tyt2y7@gmail.com>
2021-08-21 00:05:48 +0800

    Mascot

--- SeaORM/docusaurus.config.js
+++ SeaORM/docusaurus.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
   title: 'SeaORM - 🐚 An async & dynamic ORM for Rust',
-  tagline: 'SeaORM - 🐚 An async & dynamic ORM for Rust',
+  tagline: '🐚 An async & dynamic ORM for Rust',
   url: 'https://www.sea-ql.org/',
   baseUrl: '/SeaORM/',
   onBrokenLinks: 'throw',
--- SeaORM/src/pages/index.js
+++ SeaORM/src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -7,27 +6,17 @@ import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 import HomepageCompare from '../components/HomepageCompare';
 import HomepageExample from '../components/HomepageExample';
+import HomepageMascot from '../components/HomepageMascot';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={styles.heroBanner}>
-      <style>{`
-      @media only screen and (max-width: 480px) {
-        #homepageBanner {
-          display: none;
-        }
-      }
-      @media only screen and (min-width: 481px) {
-        #homepageLogo {
-          display: none;
-        }
-      }
-      `}</style>
       <div className="container">
-        <img id="homepageLogo" width="90%" src="/SeaORM/img/SeaORM logo.png" />
-        <img id="homepageBanner" width="90%" src="/SeaORM/img/SeaORM banner.png" />
-        <p className="hero__subtitle">SeaORM is an async & dynamic relational ORM<br/>to help you build light weight and concurrent web services<br/>in Rust.</p>
+        <img className={styles.homepageLogo} width="90%" src="/SeaORM/img/SeaORM logo.png" />
+        <img className={styles.homepageBanner} width="90%" src="/SeaORM/img/SeaORM banner.png" />
+        <h2 className="hero__subtitle">{siteConfig.tagline}</h2>
+        <p className="hero__subtitle">SeaORM is a relational ORM to help you<br/>build light weight and concurrent web services<br/>in Rust.</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
@@ -44,12 +33,13 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      description="🐚 An async & dynamic ORM for Rust">
+      description={siteConfig.tagline}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
         <HomepageCompare />
         <HomepageExample />
+        <HomepageMascot />
       </main>
     </Layout>
   );
