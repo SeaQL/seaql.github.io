1d6657c1ffa2d0a11896c7f9c7bbb0b4b0950aaa
Chris Tsang <chris.2y3@outlook.com>
2021-10-02 01:19:08 +0800

    Homepage example

--- SeaORM/src/components/HomepageExample.js
+++ SeaORM/src/components/HomepageExample.js
@@ -13,6 +13,30 @@ require("prismjs/components/prism-rust");
 
 const codeBlocks = [
   {
+    title: 'Entity',
+    code: `use sea_orm::entity::prelude::*;
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
+#[sea_orm(table_name = "cake")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i32,
+    pub name: String,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+    #[sea_orm(has_many = "super::fruit::Entity")]
+    Fruit,
+}
+
+impl Related<super::fruit::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Fruit.def()
+    }
+}`
+  },
+  {
     title: 'Select',
     code: `// find all models
 let cakes: Vec<cake::Model> = Cake::find().all(db).await?;
