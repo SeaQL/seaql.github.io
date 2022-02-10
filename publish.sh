d28bfe6f7a0c904bf62a149e58f39e8a44c7d2a5
Billy Chan <ccw.billy.123@gmail.com>
2022-02-10 14:46:59 +0800

    Update publish script

--- publish.sh
+++ publish.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 set -e
+
+# Build SeaORM docs
 cd SeaORM
 npm run build
 [[ -d ../docs/SeaORM ]] && rm -r ../docs/SeaORM
 mv build ../docs/SeaORM
 cd ..
+
+# Build SeaQuery docs
+cd SeaQuery
+npm run build
+[[ -d ../docs/SeaQuery ]] && rm -r ../docs/SeaQuery
+mv build ../docs/SeaQuery
+cd ..
+
+# Commit & Push
 git add docs
 git commit -m 'Publish Docs'
 git push --force origin master:gh-pages
