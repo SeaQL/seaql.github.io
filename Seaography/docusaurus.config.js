c3a39fd1cc7dad3d673faed9b1a0be1b31e7f1e0
Panagiotis Karatakis <panagiotiskaratakis@gmail.com>
2022-07-18 17:57:37 +0300

    WIP: Add Seaography documentation * Initialize project

---
+++ Seaography/docusaurus.config.js
@@ -0,0 +1,175 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Seaography - GraphQL API generator for rust',
+  tagline: 'Seaography is Rust CLI application that generates a graphql API for relational databases.',
+  url: 'https://www.sea-ql.org',
+  baseUrl: '/Seaography/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'SeaQL', // Usually your GitHub org/user name.
+  projectName: 'seaography', // Usually your repo name.
+  trailingSlash: true,
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/SeaQL/seaql.github.io/edit/master/Seaography/',
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
+        },
+        // blog: {
+          // showReadingTime: true,
+          // // Please change this to your repo.
+          // // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+        sitemap: {
+          changefreq: 'daily',
+          priority: 0.8,
+        },
+        // TODO: enable scripts
+        // scripts: [
+        //   '/sea-ql.js',
+        // ],
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
+      navbar: {
+        title: 'Seaography',
+        logo: {
+          alt: 'Seaography Logo',
+          src: 'img/SeaQL.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'index',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            to: 'https://www.sea-ql.org/SeaORM/blog/',
+            target: '_self',
+            label: 'Blog',
+            position: 'left'
+          },
+          {
+            href: 'https://crates.io/crates/seaography',
+            label: 'Crate',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/SeaQL/seaography',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Getting Started',
+                to: '/docs/index',
+              },
+              {
+                label: 'API Reference',
+                to: 'https://docs.rs/seaography/',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/SeaQL/seaography/discussions',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discord.com/invite/uCPdDXzbdv',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/sea_ql',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'About Us',
+                to: '/about-us',
+              },
+              {
+                label: 'Blog',
+                to: 'https://www.sea-ql.org/SeaORM/blog/',
+                target: '_self',
+              },
+            ],
+          },
+        ],
+        copyright: [
+          `<br/><div style="display: flex; justify-content: center;"><a href="https://github.com/SeaQL/starfish-ql/stargazers/" target="_blank"><img src="https://img.shields.io/github/stars/SeaQL/starfish-ql.svg?style=social&label=Star&maxAge=1"/></a></div>`,
+          `<br/>Copyright © ${new Date().getFullYear()} SeaQL`,
+          `<br/>Built with 🔥 by 🌊🦀🐚`,
+        ].join(''),
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      // algolia: {
+      //   appId: 'QZ9XDFJ7RQ',
+      //   apiKey: 'da8d82019ca15152ae64f9b03ce0f9bb',
+      //   indexName: 'starfishql',
+      //   contextualSearch: true,
+      // },
+      docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
+    }),
+};
+
+module.exports = config;
