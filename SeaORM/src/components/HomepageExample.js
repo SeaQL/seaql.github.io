a078019421d0affe8656ab16d598f3410166b1a5
Billy Chan <ccw.billy.123@gmail.com>
2022-08-05 18:25:24 +0800

    Update code block background color

--- SeaORM/src/components/HomepageExample.js
+++ SeaORM/src/components/HomepageExample.js
@@ -152,10 +152,10 @@ export default function HomepageCompare() {
     setMounted(true);
   }, []);
 
-  const { isDarkTheme } = useColorMode();
+  const { colorMode } = useColorMode();
   const lightModeTheme = prism.theme;
   const darkModeTheme = prism.darkTheme || lightModeTheme;
-  const prismTheme = isDarkTheme ? darkModeTheme : lightModeTheme;
+  const prismTheme = colorMode === 'dark' ? darkModeTheme : lightModeTheme;
 
   return (
     <section className={clsx('home-section', 'home-section-alt', styles.features)}>
@@ -183,7 +183,7 @@ export default function HomepageCompare() {
                       {({ className, tokens, getLineProps, getTokenProps }) => (
                         <pre
                           className={`${className}`}
-                          style={{ backgroundColor: isDarkTheme ? '#111' : '#fff' }}
+                          style={{ backgroundColor: '#292d3e' }}
                         >
                           {tokens.map((line, i) => (
                             <div {...getLineProps({ line, key: i })}>
