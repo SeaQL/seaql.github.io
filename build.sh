4a493b1d84ad831fd3601a392759c55be6b91701
Billy Chan <ccw.billy.123@gmail.com>
2023-02-22 17:44:24 +0800

    CI

--- build.sh
+++ build.sh
@@ -17,29 +17,29 @@ cd ..
 # mv build ../docs/SeaQuery
 # cd ..
 
-# # Build StarfishQL docs
-# cd StarfishQL
-# npm i
-# npm run build
-# [[ -d ../docs/StarfishQL ]] && rm -r ../docs/StarfishQL
-# mv build ../docs/StarfishQL
-# cd ..
+# Build StarfishQL docs
+cd StarfishQL
+npm i
+npm run build
+[[ -d ../docs/StarfishQL ]] && rm -r ../docs/StarfishQL
+mv build ../docs/StarfishQL
+cd ..
 
-# # Build SeaQL blog
-# cd Blog
-# npm i
-# npm run build
-# [[ -d ../docs/blog ]] && rm -r ../docs/blog
-# mv build ../docs/blog
-# cd ..
+# Build SeaQL blog
+cd Blog
+npm i
+npm run build
+[[ -d ../docs/blog ]] && rm -r ../docs/blog
+mv build ../docs/blog
+cd ..
 
-# # Build Seaography docs
-# cd Seaography
-# npm i
-# npm run build
-# [[ -d ../docs/Seaography ]] && rm -r ../docs/Seaography
-# mv build ../docs/Seaography
-# cd ..
+# Build Seaography docs
+cd Seaography
+npm i
+npm run build
+[[ -d ../docs/Seaography ]] && rm -r ../docs/Seaography
+mv build ../docs/Seaography
+cd ..
 
 # # Build SeaStreamer docs
 # cd SeaStreamer
