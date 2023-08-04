43f5474a2b32e6826031a9171104d934ca28363c
Chris Tsang <chris.2y3@outlook.com>
2023-08-04 18:47:02 +0800

    Update HomepageProducts.js

--- SeaORM/src/components/HomepageProducts.js
+++ SeaORM/src/components/HomepageProducts.js
@@ -96,10 +96,6 @@ export default function HomepageProducts() {
         <div className="row">
           <div className="col col--12">
               <h2 className="text--center">Who's using SeaORM?</h2>
-              <br/>
-              <p className="text--center">
-                The following products are powered by SeaORM:
-              </p>
           </div>
         </div>
         <div className="row">
