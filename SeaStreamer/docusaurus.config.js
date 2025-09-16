b96ed9527c80fec0e86a201933f8f5243474c254
Billy Chan <ccw.billy.123@gmail.com>
2025-09-17 07:30:00 +0800

    [SeaStreamer] Cookie Consent

--- SeaStreamer/docusaurus.config.js
+++ SeaStreamer/docusaurus.config.js
@@ -155,7 +155,15 @@ module.exports = {
       },
     ],
   ],
+  stylesheets: [
+    '/cookieconsent/cookieconsent.css',
+  ],
   scripts: [
     '/sea-ql.js',
+    {
+      type: 'module',
+      src: '/cookieconsent/cookieconsent.js',
+      async: true,
+    },
   ],
 };
