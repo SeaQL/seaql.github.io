a49e9b187531f0cc3f53bdc105a7d4a727c32a3a
Billy Chan <ccw.billy.123@gmail.com>
2025-09-17 06:48:13 +0800

    [SeaORM-Pro] Cookie Consent

--- sea-orm-pro/docusaurus.config.js
+++ sea-orm-pro/docusaurus.config.js
@@ -151,8 +151,16 @@ module.exports = {
       },
     ],
   ],
+  stylesheets: [
+    '/cookieconsent/cookieconsent.css',
+  ],
   scripts: [
     '/sea-ql.js',
+    {
+      type: 'module',
+      src: '/cookieconsent/cookieconsent.js',
+      async: true,
+    },
   ],
   plugins: [
     tailwindPlugin,
